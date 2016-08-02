bce52f24acf562672ca8a5aa89bc103f467de8ad
Calvin <calvinjhfeng@gmail.com>
2016-08-01 17:59:09 -0700

    completed grad approximation and cost function, but there's bug

--- network.js
+++ network.js
@@ -104,6 +104,18 @@ function printMat(mat) {
   }
 }
 
+function dupWeight(weights) {
+  let copy = [];
+  for (let l = 0; l < weights.length; l++) {
+    let mat = [];
+    for (let i = 0; i < weights[l].length; i++) {
+      mat.push(weights[l][i].slice());
+    }
+    copy.push(mat);
+  }
+  return copy;
+}
+
 function sigmoid(zVector) {
   let sigVector = [];
   for (let i = 0; i < zVector.length; i++) {
@@ -112,7 +124,7 @@ function sigmoid(zVector) {
   return sigVector;
 }
 
-function forwardProp(xVec) {
+function forwardProp(xVec, weights) {
   // Initialize z and put input vector into collection of activationVectors
   let z, aVecs = [xVec];
   for (let i = 0; i < weights.length; i++) {
@@ -123,10 +135,12 @@ function forwardProp(xVec) {
   return aVecs;
 }
 
-function backProp(aVecs, yVec) {
+function backProp(aVecs, yVec, weights) {
   // aVecs : Activiation vectors
   let L = aVecs.length - 1;
   let deltaVecs = [vecSubstract(aVecs[L], yVec)];
+  // construct delta vectors layer by layer, start from L
+  // although we don't need deltas[l = 0], I am putting it there as a place holder
   for (let i = L - 1; i >= 0; i--) {
     let wT = matTranspose(weights[i]);
     let a = [1].concat(aVecs[i]);
@@ -141,7 +155,7 @@ function accumDelta(bigDeltas, deltas, aVecs) {
 // Iterate through layers, denote l as layer.
 // Work on mapping from output layer to last hidden layer first.
   let L = bigDeltas.length - 1;
-  
+
   for (let i = 0; i < deltas[L + 1].length; i++) {
     let a = [1].concat(aVecs[L]);
     for (let j = 0; j < a.length; j++) {
@@ -150,6 +164,7 @@ function accumDelta(bigDeltas, deltas, aVecs) {
   }
 
   for (let l = L - 1; l >= 0; l--) {
+    // Drop the delta for the bias unit
     let delta = deltas[l + 1].slice(1);
     for (let i = 0; i < delta.length; i++) {
       let a = [1].concat(aVecs[l]);
@@ -158,23 +173,89 @@ function accumDelta(bigDeltas, deltas, aVecs) {
       }
     }
   }
+
+  return null;
 }
 
-function computePartial(trainingSetX, trainingSetY) {
+function computeBigDelta(weights, trainingSetX, trainingSetY) {
   let m = trainingSetX.length;
   let bigDeltas = [zeroMat(4, 5), zeroMat(3, 5)];
-  for (let i = 0; i < 5; i++) {
-    let activations = forwardProp(trainingSetX[i]);
-    let deltas = backProp(activations, trainingSetY[i]);
+  for (let i = 0; i < m; i++) {
+    let activations = forwardProp(trainingSetX[i], weights);
+    let deltas = backProp(activations, trainingSetY[i], weights);
     accumDelta(bigDeltas, deltas, activations);
-    console.log("Layer 1");
-    printMat(bigDeltas[0]);
-    console.log("Layer 2");
-    printMat(bigDeltas[1]);
   }
   return bigDeltas;
 }
 
+function computePartial(weights, bigDeltas, trainingSize, lambda) {
+  let m = trainingSize;
+  let partialDerivatives = [];
+  for (let l = 0; l < weights.length; l++) {
+    let partialMat = [];
+    for (let i = 0; i < weights[l].length; i++) {
+      let row = [];
+      for (let j = 0; j < weights[l][i].length; j++) {
+        if (j === 0) {
+          row.push(bigDeltas[l][i][j]/m);
+        } else {
+          row.push((bigDeltas[l][i][j]/m) + (lambda*weights[l][i][j]));
+        }
+      }
+      partialMat.push(row);
+    }
+    partialDerivatives.push(partialMat);
+  }
+  return partialDerivatives;
+}
+
+function costFunction(xVec, yVec, weights, lambda) {
+  let m = xVec.length;
+  let costSum = 0, regularizedSum = 0;
+  for (let l = 0; l < weights.length; l++) {
+    for (let i = 0; i < weights[l].length; i++) {
+      let row = weights[l][i];
+      for (let j = 0; j < row.length; j++) {
+        regularizedSum += row[j]*row[j];
+      }
+    }
+  }
+  regularizedSum *= lambda/(2*m);
+
+  for (let i = 0; i < m; i++) {
+    let aVecs = forwardProp(xVec[i], weights);
+    let y = yVec[i], h = aVecs[aVecs.length - 1];
+    for (let k = 0; k < yVec[i].length; k++) {
+      costSum += y[k]*Math.log(h[k]) + ((1 - y[k])*Math.log(1 - h[k]));
+    }
+  }
+
+  return regularizedSum - costSum/m;
+}
+
+// This shall return a list of matrix correspond to partial derivative of each
+// term inside the weight matrices.
+function approxGradient(xVec, yVec, weights, lambda, e) {
+  let approxGrad = [];
+  for (let l = 0; l < weights.length; l++) {
+    let partialMat = [];
+    for (let i = 0; i < weights[l].length; i++) {
+      let row = [];
+      for (let j = 0; j < weights[l][i].length; j++) {
+        let weightsDup1 = dupWeight(weights);
+        weightsDup1[l][i][j] += e;
+        let weightsDup2 = dupWeight(weights);
+        weightsDup2[l][i][j] -= e;
+        let approxPartial = (costFunction(xVec, yVec, weightsDup1, lambda) -
+          costFunction(xVec, yVec, weightsDup2, lambda))/(2*e);
+        row.push(approxPartial);
+      }
+      partialMat.push(row);
+    }
+    approxGrad.push(partialMat);
+  }
+  return approxGrad;
+}
 
 
 /*
@@ -189,4 +270,21 @@ function computePartial(trainingSetX, trainingSetY) {
 const weights = [randomInit(4, 5, 0.1), randomInit(3, 5, 0.1)];
 const xVec = parser.xVector;
 const yVec = parser.yVector;
-computePartial(xVec, yVec);
+let m = xVec.length;
+let bigDeltas = computeBigDelta(weights, xVec, yVec);
+let partial = computePartial(weights, bigDeltas, m, 0.01);
+console.log("Layer 1 - big Delta");
+printMat(bigDeltas[0]);
+console.log("Layer 2 - big Delta");
+printMat(bigDeltas[1]);
+
+console.log("Layer 1 - Partial");
+printMat(partial[0]);
+console.log("Layer 2 - Partial");
+printMat(partial[1]);
+
+let approx = approxGradient(xVec, yVec, weights, 0.01, 0.0001);
+console.log("Layer 1 - Approx Partial");
+printMat(approx[0]);
+console.log("Layer 2 - Approx Partial");
+printMat(approx[1]);
